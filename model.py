3318abfd0a6896b973b22005650ba18ed36235d7
Ta Duy Hai <ta.duy.hai.fami.hust@gmail.com>
2024-04-02 11:18:14 +0700

    first commit

---
+++ model.py
@@ -0,0 +1,79 @@
+import torch
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+
+class InputEmbedding(nn.Module):
+    def __init__(self, d_model, input_shape)-> None:
+        super().__init__()
+        self.d_model = d_model
+        self.input_shape = input_shape
+        self.linear = torch.randn(size=[self.input_shape[1], self.d_model], dtype = torch.float32, requires_grad= True)
+    def forward(self,x):
+        return torch.matmul(x, self.linear)     
+
+class FullyConnected(nn.Module):
+    def __init__(self, input_shape, num_of_l2,output_shape, type_active = "softmax"):
+        super().__init__()
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.num_of_l2 = num_of_l2
+        self.type_active = type_active
+        self.linear1 = nn.Linear(in_features= self.input_shape, out_features= self.num_of_l2, dtype= torch.float32)
+        self.linear2 = nn.Linear(in_features= self.num_of_l2, out_features=self.output_shape, dtype=torch.float32)
+        if self.type_active == "softmax":
+            self.active = nn.Softmax()
+        else:
+            self.active = nn.Sigmoid()
+    def forward(self, x):
+        tmp = self.linear1(x)
+        tmp = F.relu(tmp)
+        tmp = self.linear2(tmp)
+        return self.active(tmp)
+    
+class MultiHeadAtten(nn.Module):
+    def __init__(self, input_shape, d_atten, num_of_head):
+        super().__init__()
+        self.input_shape = input_shape
+        self.d_atten = d_atten
+        self.num_of_head = num_of_head
+        self.linear_q = torch.randn(size = [self.input_shape[1], self.d_atten * self.num_of_head],dtype = torch.float32, requires_grad= True)
+        self.linear_k = torch.randn(size = [self.input_shape[1], self.d_atten * self.num_of_head],dtype = torch.float32, requires_grad= True)   
+        self.linear_v = torch.randn(size = [self.input_shape[1], self.d_atten * self.num_of_head],dtype = torch.float32, requires_grad= True)
+        self.linear = torch.randn(size = [self.d_atten * self.num_of_head, self.input_shape[1]], dtype = torch.float32, requires_grad= True)
+        self.layernorm = nn.LayerNorm(self.input_shape[1])
+        
+    
+    def forward(self,x):
+        Q = torch.matmul(x, self.linear_q).transpose(0,1).view(self.num_of_head,self.d_atten,self.input_shape[0]).transpose(1,2)
+        K = torch.matmul(x, self.linear_k).transpose(0,1).view(self.num_of_head,self.d_atten,self.input_shape[0]).transpose(1,2)
+        V = torch.matmul(x, self.linear_v).transpose(0,1).view(self.num_of_head,self.d_atten,self.input_shape[0]).transpose(1,2)
+        #print(torch.matmul(torch.matmul(Q, K.transpose(1,2)).softmax(dim = 2), V))
+        V = torch.matmul(torch.matmul(Q, K.transpose(1,2)).softmax(dim = 2), V).transpose(0,1).reshape(self.input_shape[0], self.num_of_head * self.d_atten)
+        V = torch.matmul(V, self.linear)
+        V = V + x
+        return self.layernorm(V)
+
+class MainModel(nn.Module):
+    def __init__(self, input_shape, d_model, d_atten, num_of_head, num_of_l2, num_of_multi, output_shape, type_active = "softmax"):
+        super().__init__()
+        self.input_shape = input_shape
+        self.d_model = d_model
+        self.d_atten = d_atten
+        self.num_of_head = num_of_head
+        self.num_of_multi = num_of_multi
+        self.output_shape = output_shape
+        self.type_active = type_active
+        self.num_of_l2 = num_of_l2
+        self.InEm = InputEmbedding(self.d_model, self.input_shape)
+        self.FuCon = FullyConnected(self.d_model, type_active= self.type_active, output_shape= self.output_shape,num_of_l2=self.num_of_l2)
+        self.Stack_MultiHead = nn.ModuleList([MultiHeadAtten([self.input_shape[0], self.d_model], d_atten= self.d_atten, num_of_head= self.num_of_head) for _ in range(self.num_of_multi)])
+        self.AvgPooling = nn.AvgPool2d(kernel_size= [self.input_shape[0],1], padding = 0, stride=[1,1])   
+           
+    
+    def forward(self,x):                     
+        inputem = self.InEm(x)
+        for layer in self.Stack_MultiHead:
+            inputem = layer(inputem)
+        return self.FuCon(self.AvgPooling(inputem.reshape(1, inputem.shape[0], inputem.shape[1])).reshape(1, inputem.shape[1]))
+                    
---
+++ processdata.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader 
+from sklearn.model_selection import KFold
+
+
+def GetDataTrainTest(list_of_numpydata, n_splits):
+    list_data= {key: None for key,_ in list_of_numpydata.items()}
+    FiveFold= KFold(n_splits= n_splits, shuffle = True)
+    i = n_splits    
+    while True:
+        if i < n_splits:
+            train = []
+            test = []
+            for key,values in list_data.items():
+                index = next(values)
+                train += [list_of_numpydata[key][i] for i in index[0]]
+                test += [list_of_numpydata[key][i] for i in index[1]]
+            yield train, test
+            i = i+1
+        else:
+            for key, _ in list_data.items():
+                list_data[key] = FiveFold.split(list_of_numpydata[key])
+            i = 0     
+class MyData(Dataset):
+    def __init__(self, list_of_numpy_train) -> None:
+        super().__init__()
+        self.data = torch.tensor(np.array(list_of_numpy_train), dtype = torch.float32, requires_grad= False)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        label = self.data[index][-1].clone().detach().to(torch.long)
+        feature = self.data[index][:-1].clone().detach().to(torch.float32)
+        return feature, label                   
+                 
