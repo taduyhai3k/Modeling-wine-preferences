6bd3e87e3b94653efaf9e46c0481b42790681f8f
Ta Duy Hai <ta.duy.hai.fami.hust@gmail.com>
2024-04-04 22:00:49 +0700

    modified exp.py - add train_test_loop method

--- exp.py
+++ exp.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch 
 import model
 import processdata as proda
+import datetime
 
 class Exp_Basic(object):
     def __init__(self, agrs):
@@ -21,11 +22,11 @@ class Exp_Basic(object):
 class Exp_Wine(Exp_Basic):
     def __init__(self, agrs):
         super().__init__(agrs)
-        self.model = model.MainModel(self.agrs.input_shape, self.agrs.d_model, self.agrs.d_atten, self.agrs.num_of_head,
-                                     self.agrs.num_of_l2, self.agrs.num_of_multi, self.agrs.output_shape, self.type_active)
-        self.data = []
-        self.train = []
-        self.test = [] 
+        
+    def build_model(self):
+         self.model = model.MainModel(self.agrs.input_shape, self.agrs.d_model, self.agrs.d_atten, self.agrs.num_of_head,
+                                     self.agrs.num_of_l2, self.agrs.num_of_multi, self.agrs.output_shape, self.type_active)  
+                  
     def getdata(self):
         raw_data = pd.read_csv(self.agrs.filepath, sep= ",") 
         target = raw_data[raw_data.columns[self.agrs.target]]
@@ -35,6 +36,7 @@ class Exp_Wine(Exp_Basic):
             list_quality[int(target[i])].append(raw_data[i])
         self.data = proda.GetDataTrainTest(list_quality, 5)    
     def train(self):
+        self.build_model()
         self.model.train()
         loss_fn = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(self.model.parameters, lr = 0.001, betas = (0.9, 0.999))    
@@ -49,6 +51,8 @@ class Exp_Wine(Exp_Basic):
                 loss.backward()
                 optimizer.step()
             scheduler.step()    
+            if i % 4 == 0:
+                print(f"Vòng lặp thứ {i}, giá trị hàm train loss {np.mean(np.array(sum_loss))}")
         return sum_loss    
     def test(self):
         self.model.eval()
@@ -58,4 +62,28 @@ class Exp_Wine(Exp_Basic):
             output = np.array([torch.argmax(i).item() for i in output], dtype = np.int16, ndmin= 1)
             label = label.detach().numpy() 
             loss.append(np.sum(np.abs(output - label)) / len(output))
-        return np.mean(np.array(loss))    
+        print(f"Giá trị hàm test loss {np.mean(np.array(loss))}")    
+        return np.mean(np.array(loss))   
+    
+    def save(self, epoch):
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        model_dict = self.model.state_dict()  
+        checkpoint_path = f'checkpoint_epoch_{epoch}_time_{current_time}.pth'
+        checkpoint = {
+            'model_state_dict': model_dict,
+            'epoch': epoch,
+            'training_time': current_time,
+        }
+        torch.save(checkpoint, checkpoint_path)
+    
+    def train_test_loop(self):
+        array_loss_train = []
+        array_loss_test = []
+        for i in range(self.agrs.epoch):
+            data_tmp = next(self.data)
+            self.train =  proda.DataLoader(proda.MyData(data_tmp[0]), batch_size= self.agrs.batch_size, shuffle = True)
+            self.test = proda.DataLoader(proda.MyData(data_tmp[1]),batch_size= self.agrs.batch_size, shuffle = True)
+            array_loss_train.append(self.train())
+            array_loss_test.append(self.test())
+            self.save(i)
+            
